f0777919e6bd5d1da511cd1103fa58078629943e
sbozhyk <sbozhyk@intellias.com>
2020-01-23 16:27:43 +0200

    deleted temporary code

--- src/test/java/tests/loginPageTest/LoginPageTests.java
+++ src/test/java/tests/loginPageTest/LoginPageTests.java
@@ -7,10 +7,5 @@ public class LoginPageTests extends BaseTestMethods {
     @Test
     public static void loginTest(){
         System.out.println("test");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
