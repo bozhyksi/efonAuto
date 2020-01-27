ec3b22cb5398ce9108b0fe560e632f8ee8f9ad65
sbozhyk <sbozhyk@intellias.com>
2020-01-27 16:42:53 +0200

    -added allure report folders to gitignore -added allure dependencies and plugins to pom.xml

--- src/test/java/tests/loginPageTest/LoginPageTests.java
+++ src/test/java/tests/loginPageTest/LoginPageTests.java
@@ -2,18 +2,28 @@ package tests.loginPageTest;
 
 import core.configuration.customListeners.CustomListeners;
 import flow.BaseTestMethods;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+
+import static io.qameta.allure.Allure.step;
 
 @Listeners(CustomListeners.class)
 
 public class LoginPageTests extends BaseTestMethods {
 
+    @Description("Verify if user can Login with correct credentials")
     @Test(groups = {"regression", "smoke", "loginPageTests"})
     public void loginTest(){
+        step("Fill in Login");
         loginPage.fillInLogin(getLogin());
+
+        step("Fill in Pass");
         loginPage.fillInPassword(getPassword());
+
+        step("Click Login button");
         loginPage.clickLoginButton();
 
 
