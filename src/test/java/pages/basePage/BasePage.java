6312ed185955090cc00947bbb7d6f64100c5b010
sbozhyk <sbozhyk@intellias.com>
2020-01-27 12:14:14 +0200

    added locators and method to BasePage

--- src/test/java/pages/basePage/BasePage.java
+++ src/test/java/pages/basePage/BasePage.java
@@ -1,9 +1,46 @@
 package pages.basePage;
 
+import com.codeborne.selenide.SelenideElement;
 import core.configuration.fields.Fields;
 
 public class BasePage extends Fields {
 
-    //private String
+    private String buttonLogoutXpath = "//span[text()='Logout']/parent::button";
+    private String imgUpcBusinessXpath = "//a[@href=\"http://www.e-fon.ch\"]";
+    private String linkFeedbackXpath = "//span[text()='Feedback']/parent::a";
+    private String dropdownHelpXpath = "//*[@id=\"helpLabel\"]";
+    private String dropdownHelpItemKnowledgeDatabaseXpath = "//*[@class=\"dropdown open\"]//a[text()='Knowledge database']";
+    private String dropdownHelpItemSupportRequestXpath = "//*[@class=\"dropdown open\"]//a[text()='Support request']";
+    private String linkUserNameXpath = "//div[@class=\"col-xs-7\"]//a[@href=\"/portal/contact-data\"]";
+
+    public void clickButtonLogout(){
+        field(buttonLogoutXpath).click();
+    }
+
+    public void clickImgUpcBusiness(){
+        field(imgUpcBusinessXpath).click();
+    }
+
+    public void clickLinkFeedback(){
+        field(linkFeedbackXpath).click();
+    }
+
+    public void clickDropdownHelp(){
+        field(dropdownHelpXpath).click();
+    }
+
+    public void clickDropdownHelpItemKnowledgeDatabase(){
+        clickDropdownHelp();
+        field(dropdownHelpItemKnowledgeDatabaseXpath).click();
+    }
+
+    public void clickDropdownHelpItemSupportRequest(){
+        clickDropdownHelp();
+        field(dropdownHelpItemSupportRequestXpath).click();
+    }
+
+    public void clickLinkUserName(){
+        field(linkUserNameXpath).click();
+    }
 
 }
