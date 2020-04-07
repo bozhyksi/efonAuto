0930a3e2357a2f5f207472b61116366940ef9034
bozhyksi <bozhyksi1988>
2020-04-07 11:59:09 +0300

    test jenkins

--- src/test/java/tests/loginPageTest/LoginPageTests.java
+++ src/test/java/tests/loginPageTest/LoginPageTests.java
@@ -15,8 +15,7 @@ import static io.qameta.allure.Allure.step;
 public class LoginPageTests extends BaseTestMethods {
 
     @Description("Verify if user can Login/LogOut with correct credentials")
-    @Test(retryAnalyzer = RetryAnalyzer.class, groups = {"regression", "smoke", "loginPageTests"},
-            testName = "Verify if user can Login/LogOut with correct credentials")
+    @Test(retryAnalyzer = RetryAnalyzer.class, groups = {"regression", "smoke", "loginPageTests"})
     public void loginTest(){
         step("Fill in Login");
         loginPage.fillInLogin(getLogin());
